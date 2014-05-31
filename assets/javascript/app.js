b952609da0a697c67abb8adabf3b8c24b00121ac
jabez zhang <jabez128@163.com>
2014-05-31 15:42:31 +0800

    去掉哈希
    取消哈希

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
 var app = angular.module('doc',['ngRoute'])
-			     .config(function($routeProvider){
+			     .config(function($routeProvider,$locationProvider){
 					$routeProvider.when('/',{
 						templateUrl : './assets/partials/index.html',
 					}).when('/serverapi',{
 						templateUrl : './assets/partials/serverapi.html',
 						});
+					$locationProvider.html5mode(true);
 			     }).controller('main',function($scope){
 
 			     });
