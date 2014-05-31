4c1ead15c6e3bee7496400d5645edc1b9105337e
jabez zhang <jabez128@163.com>
2014-05-31 15:51:46 +0800

    添加url
    添加url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,9 @@ var app = angular.module('doc',['ngRoute'])
 						templateUrl : './assets/partials/index.html',
 					}).when('/serverapi',{
 						templateUrl : './assets/partials/serverapi.html',
-						});
-
+						}).otherwise{
+						template: '<h1>404 not found</h1>'
+						};
 					$locationProvider.hashPrefix('!');
 			     }).controller('main',function($scope){
 
