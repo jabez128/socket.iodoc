198c21896e0bdc7f3563d52c708993cb7b2e389d
jabez zhang <jabez128@163.com>
2014-05-31 15:37:40 +0800

    添加路由逻辑
    添加路由逻辑

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('doc',['ngRoute'])
+			     .config(function($routeProvider){
+					$routeProvider.when('/',{
+						templateUrl : '/assets/partials/index.html',
+					}).when('/serverapi',{
+						templateUrl : '/assets/partials/serverapi.html',
+						});
+			     }).controller('main',function($scope){
+
+			     });
